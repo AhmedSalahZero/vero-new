46bebc792bb7283575b313495792d3f853c6023c
Ahmed Salah <asalahdev5@gmail.com>
2026-08-31 19:50:42 +0300

    test1

--- app/Http/Controllers/ReadOdooInvoices.php
+++ app/Http/Controllers/ReadOdooInvoices.php
@@ -15,10 +15,15 @@ class ReadOdooInvoices extends Controller
 		$startDate = $request->get('odoo_start_date');
 		$endDate = $request->get('odoo_end_date');
 		try{
+			logger(['sapi' => PHP_SAPI, 'max_execution_time' => ini_get('max_execution_time'), 'memory_limit' => ini_get('memory_limit')]);
+
+			logger('1');
 			$odoo->startImportContracts($startDate,$endDate,$company->id);
+			logger('2');
 			$odoo->startImportInvoices($startDate,$endDate,$company->id);
-		}catch(\Exception $e){
-			session()->put('fail', $e->getMessage());
+			}catch(\Exception $e){
+				logger('3');
+				session()->put('fail', $e->getMessage());
 			return back();
 		}
 		return redirect()->back()->with('success',__('Invoices Reading Has Been Completed'));
